415297282942c57b24e17864f76af4b484fe07f9
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2021-04-21 17:09:59 -0700

    3.1 completion

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+  {
+    id: 1,
+    name: "Katie",
+    number: "555-555-5555"
+  },
+  {
+    id: 2,
+    name: "Adrien",
+    number: "666-666-6666"
+  },
+  {
+    id: 3,
+    name: "Sparky",
+    number: "777-777-7777"
+  },
+  {
+    id: 4,
+    name: "Abby",
+    number: "888-888-8888"
+  },
+]
+
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
